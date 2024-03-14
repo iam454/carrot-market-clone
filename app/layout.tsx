775133b8d928929a49497df9e2479b903e8248b1
iam454 <realseogy@gmail.com>
2024-03-14 14:14:46 +0900

    feat: add home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-neutral-900 text-white max-w-screen-sm mx-auto`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="flex flex-col justify-between items-center min-h-screen p-6 min-w-[360px]">
+      <div className="*:font-medium flex flex-col items-center my-auto gap-2">
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl">당근</h1>
+        <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
+      </div>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link
+          href="/create-account"
+          className="w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors"
+        >
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link href="/login" className="hover:underline">
+            로그인
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
