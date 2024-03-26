ab423f60a6027ac5511d86922bbdefd5331a0f59
iam454 <realseogy@gmail.com>
2024-03-26 15:33:09 +0900

    feat: zod validation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,12 +2,41 @@
 
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(8),
-  confirmPassword: z.string().min(8),
-});
+const passwordRegex = new RegExp(
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/
+);
+
+const checkPasswords = ({
+  password,
+  confirmPassword,
+}: {
+  password: string;
+  confirmPassword: string;
+}) => password === confirmPassword;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username must be a string.",
+        required_error: "Username must be required.",
+      })
+      .min(3)
+      .max(10),
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(8)
+      .regex(
+        passwordRegex,
+        "A password must have lowercase, UPPERCASE, a number and special characters."
+      ),
+    confirmPassword: z.string().min(8),
+  })
+  .refine(checkPasswords, {
+    message: "Both passwords must be the same.",
+    path: ["confirmPassword"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
